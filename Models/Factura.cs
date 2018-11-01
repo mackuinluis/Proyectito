96bc6c38f7e857782c66a06f402f772e56c71087
Mackuin <mackuin_luis@hotmail.com>
2018-11-01 10:09:53 -0500

    Mejoras en factura

--- Models/Factura.cs
+++ Models/Factura.cs
@@ -7,22 +7,20 @@ namespace demomvc.Models
     public class Factura
     {
         public int ID { get; set; }
-        [Required(ErrorMessage = "Please enter last name")]
-		[StringLength(40)]
-        public string CodigoFactura { get; set; }
+        public int CodigoFactura { get; set; }
         public string PlacaTracto { get; set; }
         public string PlacaCarreta { get; set; }
-        public string CodigoConductor { get; set; }
+        public int CodigoConductor { get; set; }
         public string Origen { get; set; }
         public string FechaSalida { get; set; }
         public string Destino { get; set; }
         public string FechaDestino { get; set; }
-        public string CodigoCarga { get; set; }
+        public int CodigoCarga { get; set; }
         public string FechaEntregaDocumento { get; set; }
-        public string MontoFacturado { get; set; }
+        public int MontoFacturado { get; set; }
         public string Estado { get; set; }
         public string FechaCobrado { get; set; }
-        public string MontoCobrado { get; set; }
+        public int MontoCobrado { get; set; }
 
 
     }
