7a63779d7e981200f84b231dd7e4f869d834d912
banezaporroa <banezaporroa.es@gmail.com>
2018-11-06 23:56:40 -0500

    modifiacion de mensajes en el usuario(correo/contraseña)

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -7,20 +7,20 @@ namespace demomvc.Models
     public class Usuario
     {
         public int ID { get; set; }
-        [Required(ErrorMessage = "Ingrese Nombre")]
+        [Required(ErrorMessage = "Ingrese Nombres")]
 		[StringLength(40)]
         public string Nombre { get; set; }
-        [Required(ErrorMessage = "Ingrese Apellido")]
+        [Required(ErrorMessage = "Ingrese Apellidos")]
         public string Apellidos { get; set; }
         [Required(ErrorMessage = "Ingrese Usuario")]
         public string Usu { get; set; }
         [Required(ErrorMessage = "Ingrese Contraseña")]
         public string Contraseña { get; set; }
-        [Compare(nameof(Contraseña),ErrorMessage = "Confirmación Incorrecta")] 
+        [Compare(nameof(Contraseña),ErrorMessage = "Contraseña incorrecta")] 
         public string ConfirmarContraseña { get; set;}
         [Required(ErrorMessage = "Ingrese Correo")]
         public string Correo { get; set; }
-        [Compare(nameof(Correo),ErrorMessage = "Confirmación Incorrecta")] 
+        [Compare(nameof(Correo),ErrorMessage = "Esta cuenta de correo no existe. Indica una cuenta diferente ")] 
         public string ConfirmarCorreo { get; set;}
         [Required(ErrorMessage = "Ingrese Celular")]
         public int Celular { get; set; }
